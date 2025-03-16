32aba6c0089ab2e5d153504d2e6716dfacdd4cd8
sajjad akbari <seokarid@gmail.com>
2025-03-16 11:29:11 +0000

    توضیح مختصر درباره تغییرات

---
+++ seokar/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sajjadakbari/seokar',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2b4b1dbff1abd0553291a22dff3b17c43708fa06',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sajjadakbari/seokar' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2b4b1dbff1abd0553291a22dff3b17c43708fa06',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
