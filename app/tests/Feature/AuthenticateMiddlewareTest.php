d8fa9c2e43fbd8db3cfd745fa3acef27487d3ab7
sajjad akbari <seokarid@gmail.com>
2025-03-16 13:07:37 +0330

    Update AuthenticateMiddlewareTest.php

--- app/tests/Feature/AuthenticateMiddlewareTest.php
+++ app/tests/Feature/AuthenticateMiddlewareTest.php
@@ -1,1 +1,16 @@
 
+public function testUnauthenticatedAccess()
+{
+    $response = $this->get('/dashboard');
+
+    $response->assertRedirect(route(app()->getLocale() . '.login'));
+}
+
+public function testAuthenticatedAccess()
+{
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->get('/dashboard');
+
+    $response->assertOk();
+}
